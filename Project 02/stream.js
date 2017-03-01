be0d26d3cd74eb185e1c4b1c4a0d74f04ca9c521
Rehan Butt <rab@Rehans-MBP-2.wv.cc.cmu.edu>
2017-03-01 15:45:23 -0500

    Project 2 updates

--- Project 02/stream.js
+++ Project 02/stream.js
@@ -2,6 +2,9 @@ var Twitter = require('twitter'); // for the Twitter API
 var env = require('dotenv').config(); // for loading API credentials
 var moment = require('moment'); // for displaying dates nicely
 
+var count = 0;
+var tv = new GPIO(18, 'out');
+
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -14,21 +17,29 @@ var args = process.argv.slice(2);
 
 
 // set up a stream
-var streamer = client.stream('statuses/filter', {track: args[0]});
+var streamer = client.stream('statuses/filter', {track: args[0]}); //ask user for a keyword
 
 
-streamer.on('data', function(tweet) {
+streamer.on('data', function(tweet) { //runs on every incoming tweet
 
-    //write function here
+    if(){ //time between tweets is less then 3 seconds
+       
+    }
+    count++;
+    if(count > 1000)
+      {
+        tvOff();
+      }
     var name = tweet.user.screen_name;
     var text = tweet.text;
     var date = moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY");
 
-    console.log(">    @" + name + " said: " + text + ", on " + date.format("YYYY-MM-DD") + " at " + date.format("h:mma"));
+//    console.log(">    @" + name + " said: " + text + ", on " + date.format("YYYY-MM-DD") + " at " + date.format("h:mma"));
 
 });
 
 
 //write helper
-if
-    powerSwitch.writeSync(1);
+function tvOff(){
+    tv.writeSync(1); //writes to powertail
+}
